fd9cc3208d17ff4ea8d8d6640beacb2483e53814
AlexLada <vizuvi2@mail.ru>
2023-05-23 00:13:54 +0500

    last commit

--- pages/index.js
+++ pages/index.js
@@ -43,10 +43,78 @@ export default function Index() {
   };
 
   return (
-    <div className="flex h-screen w-full" >
-      <div className="flex-1 bg-center">
-      <img className="" src="bg.png" alt="bg"/>
-      </div>    
+    <div>
+      {/* <img src="1.png" alt="tailwind logo" className=""/> */}
+
+      <div className="grid grid-cols-5 grid-rows-5 gap-4 lg:gap-6 sm:p-0 p-8 min-h-screen bg-gradient-to-b from-black via-purple-100 to-[#191970]">
+        <div className="col-span-5 row-start-2 col-start-1  text-lg font-bold text-center p-2 rounded-2xl bg-yellow-400/75 shadow-2xl shadow-[#FFD700] hover:scale-125 xl:hover:scale-125 lg:hover:scale-125 md:hover:scale-125 transition-all transform duration-500">
+          <h1 className="my-auto xl:text-2xl drop-shadow-2xl shadow-black">
+            <font size="24" color="#6A5ACD" face="Playball">
+              С наступающим 2023 годом!
+            </font>
+          </h1>
+          <img className=" mx-auto xl:11/12" src="cmp.png" alt="gift" />
+        </div>
+
+        <div className="my-auto col-span-3 row-start-3 col-start-1 rounded-3xl bg-yellow-400/75 shadow-2xl shadow-[#FFD700] hover:shadow-2xl hover:scale-125 xl:hover:scale-125 lg:hover:scale-125 md:hover:scale-125 transition-all transform duration-500 ">
+          <h1 className=" flex justify-evenly  drop-shadow-2xl shadow-black">
+            <font color="#6A5ACD" face="Playball">
+              Тыкни на эту картинку
+            </font>
+          </h1>
+          <img
+            className="rounded-t-3xl rounded-3xl mx-auto p-0 scale-50 hover:scale-150 xl:hover:scale-125 lg:hover:scale-125 md:hover:scale-125 sm:hover:scale-150 transition-all transform duration-700 "
+            src="ph3.png"
+            alt="photo"
+          />
+        </div>
+
+        <div className="col-span-2 col-start-4 row-start-3 rounded-3xl bg-yellow-400/75 shadow-2xl shadow-[#FFD700] hover:scale-125 xl:hover:scale-125 lg:hover:scale-125 md:hover:scale-125 transition-all transform duration-500">
+        <h1 className="text-center text-2xl pt-16">
+            <font color=".8135L13" face="Playball">
+              Александр Лада поздравляет всех с новым годом!
+            </font>
+          </h1>
+          
+        </div>
+
+        <div className="col-span-3 col-start-1 row-start-4 rounded-3xl bg-yellow-400/75 shadow-2xl shadow-[#FFD700] hover:scale-125 xl:hover:scale-125 lg:hover:scale-125 md:hover:scale-125 transition-all transform duration-500">
+        <Countdown date={"2023-01-01T00:00:00"} renderer={renderer}>
+        </Countdown>
+
+        <img src="ph.png" alt="photo" className="w-24 rounded-3xl mx-auto shadow-2xl shadow-[#191970]"/>
+        </div>
+        
+
+        <div className="col-span-2 col-start-4 row-start-4 rounded-3xl bg-yellow-400/75 shadow-2xl shadow-[#FFD700] hover:scale-125 xl:hover:scale-125 lg:hover:scale-125 md:hover:scale-125 transition-all transform duration-500">
+          <h1 className="flex justify-evenly drop-shadow-2xl shadow-black">
+            <font color="#6A5ACD" face="Playball">
+              И на эту
+            </font>
+          </h1>
+          <img
+            className="rounded-t-3xl rounded-3xl mx-auto p-0 mb-2 scale-100 hover:scale-150 xl:hover:scale-125 lg:hover:scale-125 md:hover:scale-125 sm:hover:scale-150 transition-all transform duration-700"
+            src="ph4.png"
+            alt="photo"
+          />
+          
+        </div>
+        <img
+          className="col-start-5 row-start-1 w-44"
+          src="moon.png"
+          alt="moon"
+        />
+
+        <Star />
+        <Star1 />
+        <Star2 />
+        <Star3 />
+        <Star4 />
+        <Star5 />
+        <Star6 />
+
+        <div className="col-start-1 row-start-2 col-span-5 row-span-3 rounded-3xl  bg-red-500/75 shadow-2xl  shadow-[#CD5C5C]"></div>
+      </div>   
     </div>
   );
 }
